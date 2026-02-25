789d265c6635a0ee5af4b6930c32b58ec9729fd6
taekwon1243z-gif <taekwon1243z@gmail.com>
2026-02-25 22:12:35 +0900

    민감 파일 gitignore 처리

--- get_token2.py
+++
@@ -1,9 +0,0 @@
-import requests
-res = requests.post('https://kauth.kakao.com/oauth/token', data={
-    'grant_type': 'authorization_code',
-    'client_id': 'd0eb21ff2a868efe8d8cc4b2f2c8d0e3',
-    'client_secret': 'lvkuIZvwFrgzUV1wzIt8PsU7rTB4FEgW',
-    'redirect_uri': 'https://example.com',
-    'code': 'VAOR3xdEDePT0tLOgy3HQf1V8arRjZD3fn5qs8hMBFpmyo3BjrllkwAAAAQKDSBaAAABnH5blwAWphHJzwXJqw'
-})
-print(res.json())
--- test_kakao.py
+++
@@ -1,7 +0,0 @@
-import requests, json
-res = requests.post(
-    'https://kapi.kakao.com/v2/api/talk/memo/default/send',
-    headers={'Authorization': 'Bearer GFfwyYROCeDCvWThnm6yWppWdsU-xFcDAAAAAQoNGZAAAAGcflvoV0PPWzORmYVE'},
-    data={'template_object': json.dumps({"object_type": "text", "text": "MDD봇 테스트!", "link": {}})}
-)
-print(res.json())
